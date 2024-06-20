8c9c7141ff747b64f65c40d88983a62f1be7fde2
sanjith-devassure <sanjith@devassure.io>
2024-06-20 19:00:09 +0530

    ertretete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use git2::build::CheckoutBuilder;
 use git2::Branch;
 use git2::BranchType;
+use git2::CredentialHelper;
 use git2::Oid;
 use git2::Repository;
 use git2::StatusOptions;
@@ -9,6 +10,8 @@ use std::path::Path;
 use std::collections::HashMap;
 use serde_json::json;
 use chrono::DateTime;
+use git2::RemoteCallbacks;
+use git2::Cred;
 
 fn is_git_repo(proj_path: String) -> bool {
     let path = Path::new(&proj_path);
@@ -29,6 +32,17 @@ fn is_git_repo(proj_path: String) -> bool {
     return true;
 }
 
+fn create_callbacks(repo: &Repository) -> RemoteCallbacks {
+    let mut callbacks = RemoteCallbacks::new();
+
+    callbacks.credentials(|_url, username_from_url, _allowed_types| {
+        Cred::credential_helper(&repo.config().unwrap(), "https://github.com/sanjith-s/hello-rust.git", None)});
+
+    // &callbacks.credentials(|_str, _str_opt, _cred_type| {
+    //     Cred::credential_helper(&repo.config().unwrap(), "https://github.com/sanjith-s/hello-rust.git", None)
+    // });
+    callbacks
+}
 
 fn main() {
 
@@ -37,30 +51,35 @@ fn main() {
         Err(e) => panic!("failed to init: {}", e),
     };
     
+    // let cred = git2::Cred::credential_helper(&repo.config().unwrap(), "https://github.com/sanjith-s/hello-rust.git", Some("sanjith-s")).unwrap();
+    // dbg!(cred.credtype());
 
-    let config  = repo.config().unwrap();
-
-    let mut entries = config.entries(None).unwrap();
-    while let Some(entry) = entries.next() {
-        let entry = entry.unwrap();
-        println!("{} => {}", entry.name().unwrap(), entry.value().unwrap());
-    }    
-    
-    // let repo_remotes = repo.remotes().unwrap();
-
-    // let found_origin = repo_remotes.iter().any(|remote| {
-    //     remote.unwrap() == "origin"
-    // });
-    
-    // if !found_origin {
-    //     println!("No origin remote found");
-    //     return;
+    // let mut cred_helper = git2::CredentialHelper::new("https://github.com/sanjith-s/hello-rust.git");
+    // // cred_helper.username(Some("sanjith-s")); 
+    // let result = cred_helper.execute().unwrap();
+    // // if result.is_some() {
+    //     dbg!(result);
     // }
+    
+    let repo_remotes = repo.remotes().unwrap();
 
-    // let mut remote_obj = repo.find_remote("origin").unwrap();
-    // dbg!(remote_obj.url().unwrap());
+    let found_origin = repo_remotes.iter().any(|remote| {
+        remote.unwrap() == "origin"
+    });
+    
+    if !found_origin {
+        println!("No origin remote found");
+        return;
+    }
 
-    // match remote_obj.connect_auth(git2::Direction::Push, None, None) {
+    let mut remote_obj = repo.find_remote("origin").unwrap();
+    dbg!(remote_obj.url().unwrap());
+
+    // remote_obj.fetch(&["main"], None, None);
+
+    // remote_obj.push(&[&"main"], None).unwrap();
+    
+    // match remote_obj.connect_auth(git2::Direction::Push, Some(create_callbacks(&repo)), None) {
     //     Ok(_) => println!("Connected to remote"),
     //     Err(e) => {
     //         println!("Failed to connect to remote: {:?}", e.code());
@@ -68,6 +87,16 @@ fn main() {
     //     }
     
     // };
+    let mut push_options = git2::PushOptions::default();
+    let mut callbacks = create_callbacks(&repo);
+    callbacks.push_update_reference(|reference,error|{
+        println!("ref = {}, error = {:?}", reference, error);
+        Ok(())
+    });
+    push_options.remote_callbacks(callbacks);
+
+    remote_obj.push(&["refs/heads/main:refs/remotes/origin/main"], Some(&mut push_options)).unwrap();
+    // dbg!(remote_obj.connected());
     
     // if remote_obj.connected() {
 
