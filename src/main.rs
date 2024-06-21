7df76c8003f182458f9c81c3b23f4d082b115f23
sanjith-devassure <sanjith@devassure.io>
2024-06-21 15:31:17 +0530

    updates

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use git2::build::CheckoutBuilder;
 use git2::Branch;
 use git2::BranchType;
 use git2::CredentialHelper;
+use git2::ErrorCode;
 use git2::Oid;
 use git2::Repository;
 use git2::StatusOptions;
@@ -51,106 +52,57 @@ fn main() {
         Err(e) => panic!("failed to init: {}", e),
     };
     
-    // let cred = git2::Cred::credential_helper(&repo.config().unwrap(), "https://github.com/sanjith-s/hello-rust.git", Some("sanjith-s")).unwrap();
-    // dbg!(cred.credtype());
-
-    // let mut cred_helper = git2::CredentialHelper::new("https://github.com/sanjith-s/hello-rust.git");
-    // // cred_helper.username(Some("sanjith-s")); 
-    // let result = cred_helper.execute().unwrap();
-    // // if result.is_some() {
-    //     dbg!(result);
-    // }
     
-    // let repo_remotes = repo.remotes().unwrap();
+    let repo_remotes = repo.remotes().unwrap();
 
-    // let found_origin = repo_remotes.iter().any(|remote| {
-    //     remote.unwrap() == "origin"
-    // });
+    let found_origin = repo_remotes.iter().any(|remote| {
+        remote.unwrap() == "origin"
+    });
     
-    // if !found_origin {
-    //     println!("No origin remote found");
-    //     return;
-    // }
+    if !found_origin {
+        println!("No origin remote found");
+        return;
+    }
 
-    // let mut remote_obj = repo.find_remote("origin").unwrap();
-    // dbg!(remote_obj.url().unwrap());
+    let mut remote_obj = repo.find_remote("origin").unwrap();
+    dbg!(remote_obj.url().unwrap());
 
-    // remote_obj.fetch(&["main"], None, None);
+    let mut local_branch = repo.find_branch("simple", git2::BranchType::Local).unwrap();
+    let remote_branch = match local_branch.upstream() {
+        Ok(branch) => {},
+        Err(e) => {
 
-    // remote_obj.push(&[&"main"], None).unwrap();
+            if e.code() == ErrorCode::NotFound {
+                local_branch.set_upstream(Some("simple")).unwrap();
+            } else {
+                let json_str = json!({
+                    "code": e.code() as i32,
+                    "message": "Remote upstream branch not found",
+                }).to_string();
+                dbg!(json_str);
+                return;
+            }
+           
+        }
+    };
     
-    // match remote_obj.connect_auth(git2::Direction::Push, Some(create_callbacks(&repo)), None) {
-    //     Ok(_) => println!("Connected to remote"),
-    //     Err(e) => {
-    //         println!("Failed to connect to remote: {:?}", e.code());
-    //         return;
-    //     }
-    
-    // };
 
     // Push
-    // let mut push_options = git2::PushOptions::default();
-    // let mut callbacks = create_callbacks(&repo);
+    let mut fetch_options = git2::FetchOptions::default();
+    let callbacks = create_callbacks(&repo);
     // callbacks.push_update_reference(|reference,error|{
     //     println!("ref = {}, error = {:?}", reference, error);
     //     Ok(())
     // });
-    // push_options.remote_callbacks(callbacks);
+    fetch_options.remote_callbacks(callbacks);
 
-    // remote_obj.push(&["refs/heads/main:refs/heads/main"], Some(&mut push_options)).unwrap();
+    remote_obj.fetch(&["main"], None, None).unwrap();
+
+    let fetch_head = repo.refname_to_id("FETCH_HEAD").unwrap();
+    let origin_main = repo.refname_to_id("refs/remotes/origin/main").unwrap();
+
+    dbg!(fetch_head, origin_main);
     
-    let local_branch = repo.find_branch("main", git2::BranchType::Local).unwrap();
-    let remote_branch = match local_branch.upstream() {
-        Ok(branch) => branch,
-        Err(e) => {
-            let json_str = json!({
-                "code": e.code() as i32,
-                "message": "Unable to access Remote branch"
-            }).to_string();
-            return;
-        }
-    };
-
-    let mut walker = repo.revwalk().unwrap();
-
-    let local_name = local_branch.name().unwrap().unwrap();
-    let remote_name = remote_branch.name().unwrap().unwrap();
-
-    let commit_range = format!("{}..{}", remote_name, local_name);
-    walker.push_range(&commit_range).unwrap();
-
-    dbg!(local_name, remote_name);
-
-    for commit in walker {
-        let commit = commit.unwrap();
-        let commit = repo.find_commit(commit).unwrap();
-        let commit_id = commit.id();
-        let commit_id = commit_id.to_string();
-        let commit_message = commit.message().unwrap();
-        let commit_message = commit_message.trim();
-        let commit_author = commit.author();
-        let commit_author = commit_author.name().unwrap();
-        let commit_time = commit.time();
-        let commit_time = commit_time.seconds();
-        let commit_time = DateTime::from_timestamp(commit_time, 0).unwrap();
-
-        println!("Commit id: {:?} - Commit Message: {:?}", commit_id, commit_message);
-
-    }
-
-    // dbg!(remote_obj.connected());
-    
-    // if remote_obj.connected() {
-
-    //     let ref_specs = remote_obj.refspecs();
-
-    //     for spec in ref_specs {
-    //         println!("Ref spec: {:?}", spec.str());
-    //     }
-    //     println!("Connected to remote");
-    // } else {
-    //     println!("Not connected to remote");
-    // }
-
+ 
     return;
 }
